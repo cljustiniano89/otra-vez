5a9fa5014530fe95619dd3b25d4c78e0a2c47a11
cljustiniano89 <135278712+cljustiniano89@users.noreply.github.com>
2023-06-07 21:33:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function () {
+        document.querySelector(".nav-links")
+            .classList.toggle("nav-links-responsive")
+    })
